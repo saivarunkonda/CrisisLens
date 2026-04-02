a45e47b72f85b95b988c9acbd702c9f9e4b42892
saivarunkonda <saivarunkonda007@gmail.com>
2026-04-02 10:47:50 +0530

    Add GitHub OIDC provider resource in Terraform

--- infra/terraform/github_oidc_role.tf
+++ infra/terraform/github_oidc_role.tf
@@ -3,12 +3,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
 data "aws_iam_policy_document" "github_actions_trust" {
   statement {
     effect = "Allow"
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
